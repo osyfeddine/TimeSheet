874e3cd82fb6d3e463b34b517ffe2f4079ef8f04
o.syfedine <o.syfedine@gmail.com>
2018-12-10 11:30:46 +0100

    test

---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Timesheet.Startup))]
+namespace Timesheet
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
